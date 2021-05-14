4ccc21d6eafdcf7c22f06bdc94be02c6d4fd5fbc
arjunMee <70093722+arjunMee@users.noreply.github.com>
2021-05-15 04:33:40 +0530

    Update index.js

--- customise product form/index.js
+++ customise product form/index.js
@@ -1,33 +1,47 @@
 // change background color when clicking on
-let switchBackgroundColor = document.querySelectorAll('.desc')
+let switchBackgroundColor1 = document.querySelectorAll('.desc1')
+let switchBackgroundColor2 = document.querySelectorAll('.desc2')
+let switchBackgroundColor3 = document.querySelectorAll('.desc3')
 
-function remove() {
-  switchBackgroundColor.forEach((single) => {
+function remove(position) {
+  position.forEach((single) => {
     single.classList.remove('green')
   })
 }
 
-switchBackgroundColor.forEach((e) => {
-  e.addEventListener('click', () => {
-    remove()
-    e.classList.add('green')
+switchBackgroundColor1.forEach((box) => {
+  box.addEventListener('click', () => {
+    remove(switchBackgroundColor1)
+    box.classList.add('green')
+  })
+})
+switchBackgroundColor2.forEach((box) => {
+  box.addEventListener('click', () => {
+    remove(switchBackgroundColor2)
+    box.classList.add('green')
+  })
+})
+switchBackgroundColor3.forEach((box) => {
+  box.addEventListener('click', () => {
+    remove(switchBackgroundColor3)
+    box.classList.add('green')
   })
 })
 
 // tab animation code
 function openCity(evt, cityName) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(cityName).style.display = "block";
-    evt.currentTarget.className += " active";
+  var i, tabcontent, tablinks
+  tabcontent = document.getElementsByClassName('tabcontent')
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = 'none'
   }
-  
-  // Get the element with id="defaultOpen" and click on it
-  document.getElementById("defaultOpen").click();
+  tablinks = document.getElementsByClassName('tablinks')
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(' active', '')
+  }
+  document.getElementById(cityName).style.display = 'block'
+  evt.currentTarget.className += ' active'
+}
+
+// Get the element with id="defaultOpen" and click on it
+// document.getElementById('defaultOpen').click()
